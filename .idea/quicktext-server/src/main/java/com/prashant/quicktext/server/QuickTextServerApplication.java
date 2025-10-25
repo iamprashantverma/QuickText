6bbd241a1f7294d7875b28fc12ffa2e7f346396a
iamprashantverma <prashant2k26@gmail.com>
2025-10-25 16:04:05 +0530

    merge conflicts

---
+++ .idea/quicktext-server/src/main/java/com/prashant/quicktext/server/QuickTextServerApplication.java
@@ -0,0 +1,13 @@
+package com.prashant.quicktext.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickTextServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickTextServerApplication.class, args);
+	}
+
+}
---
+++ .idea/quicktext-server/src/test/java/com/prashant/quicktext/server/QuickTextServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prashant.quicktext.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickTextServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

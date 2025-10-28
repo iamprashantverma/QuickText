ff5ac1f2d857c973b17bb64afc369ad40d37d96e
iamprashantverma <prashant2k26@gmail.com>
2025-10-28 23:34:23 +0530

    feat : cloudinary added

--- quicktext-server/src/main/java/com/prashant/quicktext/server/configs/AppConfig.java
+++ quicktext-server/src/main/java/com/prashant/quicktext/server/configs/AppConfig.java
@@ -1,6 +1,9 @@
 package com.prashant.quicktext.server.configs;
 
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -19,4 +22,19 @@ public class AppConfig {
         return new BCryptPasswordEncoder();
     }
 
+    @Value("${cloudinary.cloud-name}")
+    private String cloudName;
+    @Value("${cloudinary.api-key}")
+    private String apiKey;
+    @Value("${cloudinary.api-secret}")
+    private String apiSecret;
+
+    @Bean
+    public Cloudinary cloudinary() {
+        return new Cloudinary(ObjectUtils.asMap(
+                "cloud_name", cloudName,
+                "api_key", apiKey,
+                "api_secret", apiSecret
+        ));
+    }
 }

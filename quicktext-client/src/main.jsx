0d26b2f495f3b2e03a9fc5945fb01c4915f03a5a
iamprashantverma <prashant2k26@gmail.com>
2025-10-31 23:43:43 +0530

    fix: add proper Vercel Analytics integration for Vite

--- quicktext-client/src/main.jsx
+++ quicktext-client/src/main.jsx
@@ -1,12 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { Toaster } from "react-hot-toast";
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { Toaster } from 'react-hot-toast';
+import App from './App.jsx';
+import { Analytics } from '@vercel/analytics/react';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
     <Toaster position="top-center" reverseOrder={false} />
-  </StrictMode>,
-)
+    <Analytics />
+  </StrictMode>
+);

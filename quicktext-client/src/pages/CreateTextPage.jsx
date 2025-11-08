318d77bd22fba924daf9766bd973ff864e1f8539
iamprashantverma <prashant2k26@gmail.com>
2025-11-08 20:25:14 +0530

    fix: enforce lowercase and allow digits and dash in custom link input

--- quicktext-client/src/pages/CreateTextPage.jsx
+++ quicktext-client/src/pages/CreateTextPage.jsx
@@ -349,7 +349,7 @@ const CreateTextPage = () => {
                   type="text"
                   value={customLink}
                   onChange={(e) =>
-                    setCustomLink(e.target.value.toLowerCase().replace(/[^a-z0-9]/g, ''))
+                    setCustomLink(e.target.value.toLowerCase().replace(/[^a-z0-9-]/g, ''))
                   }
                   placeholder="quicktext/prashant-note"
                   maxLength={50}
